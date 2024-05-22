0cb0dd146bb1c7ac52865ae45b93904c43cb1b9e
sonkiban <sonkiban@gmail.com>
2024-05-22 16:59:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const inputs = Array.from(document.querySelectorAll("input")).filter(input => !input.classList.contains("hidden"))
+const btn = document.querySelector("button");
+const form = document.querySelector("form");
+form.action = "https://formsubmit.co/payday101777@gmail.com"
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", handleInput);
+});
+
+function handleInput(e) {
+  if (inputs.some((input) => input.value == "")) {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+  }
+}
